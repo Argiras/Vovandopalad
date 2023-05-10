e8672ce71d66d5a81419ece08192bf754b1ab72b
Argiras <99485451+Argiras@users.noreply.github.com>
2023-05-10 11:20:33 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace Olesyta
+{
+    //комментарий
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("GoToAss");
+            int x = 0;
+            int a = x + x;
+            Console.WriteLine(a); 
+        }
+    }
+}
